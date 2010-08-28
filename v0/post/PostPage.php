2c766fa2c71ef4285f108aff29ff4b3682c25d84
dvdcerizza <dvd.cerizza@live.it>
2010-08-28 16:48:06 +0200

    Creata classe PostPage

---
+++ v0/post/PostPage.php
@@ -0,0 +1,74 @@
+<?php
+require_once("post/PostManager.php");
+
+class PostPage {
+	static function showPost() {
+
+	}
+
+	static function showForLikelihood() {
+
+	}
+	
+	static function showCommentForm() {
+	?>
+        <form name="" action="" method="get">
+            <input type="submit" value="">
+        </form>
+        <?php
+	}
+
+	static function submitCommentForm() {
+
+	}
+
+	static function showVoteForm() {
+	?>
+        <form name="" action="" method="get">
+            <input type="submit" value="">
+        </form>
+        <?php
+	}
+
+	static function submitVoteForm() {
+
+	}
+
+	static function showEditForm() {
+	?>
+        <form name="" action="" method="get">
+            <input type="submit" value="">
+        </form>
+        <?php
+	}
+
+	static function submitEditForm() {
+
+	}
+
+	static function showCollectionForm() {
+	?>
+        <form name="" action="" method="get">
+            <input type="submit" value="">
+        </form>
+        <?php
+	}
+
+	static function submitCollectionForm() {
+
+	}
+
+	static function showContestForm() {
+	?>
+        <form name="" action="" method="get">
+            <input type="submit" value="">
+        </form>
+        <?php
+	}
+
+	static function submitContestForm() {
+
+	} 
+}
+
+?>
