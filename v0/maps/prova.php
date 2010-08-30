c27106576ef1291d678fe14e6bdba67153cf5d76
samvise85 <luca.samvise@gmail.com>
2010-08-30 21:11:30 +0200

    Aggiunta pagina di prova che utilizza le API Google Maps. Provatela, cambiatela, distruggetela… tanto è codice copiato e incollato!

---
+++ v0/maps/prova.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+<style type="text/css">
+  html { height: 100% }
+  body { height: 100%; margin: 0px; padding: 0px }
+  #map_canvas { height: 100% }
+</style>
+<script type="text/javascript"
+    src="http://maps.google.com/maps/api/js?sensor=false">
+</script>
+<script type="text/javascript">
+  var geocoder;
+  var map;
+  function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var myOptions = {
+      zoom: 11,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    codeAddress();
+  }
+
+  function codeAddress() {
+    var address = document.getElementById("address").value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map, 
+            position: results[0].geometry.location
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }
+
+</script>
+<body onload="initialize()">
+ <div id="map_canvas" style="width: 100%; height: 480px;"></div>
+  <div>
+    <input id="address" type="textbox" value="Milano">
+    <input type="button" value="Encode" onclick="codeAddress()">
+  </div>
+</body>
