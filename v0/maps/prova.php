786ff88cc7222cadae9f6eced3818ef98418ac2b
samvise85 <luca.samvise@gmail.com>
2010-08-31 19:37:51 +0200

    Modificata la pagina della mappa perché carichi in automatico il luogo dove si trova l'utente.

--- v0/maps/prova.php
+++ v0/maps/prova.php
@@ -7,24 +7,72 @@
   body { height: 100%; margin: 0px; padding: 0px }
   #map_canvas { height: 100% }
 </style>
-<script type="text/javascript"
-    src="http://maps.google.com/maps/api/js?sensor=false">
-</script>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="http://code.google.com/apis/gears/gears_init.js"></script>
 <script type="text/javascript">
   var geocoder;
   var map;
   function initialize() {
     geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
     var myOptions = {
       zoom: 11,
-      center: latlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-    codeAddress();
+  
+    var auto = true;
+    if(auto) {  
+      if(navigator.geolocation) {
+        browserSupportFlag = true;
+        navigator.geolocation.getCurrentPosition(function(position) {
+          initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+          contentString = "Location found using W3C standard";
+          map.setCenter(initialLocation);
+          map.setZoom(16);
+          //infowindow.setContent(contentString);
+          //infowindow.setPosition(initialLocation);
+          //infowindow.open(map);
+        }, function() {
+          handleNoGeolocation(browserSupportFlag);
+        });
+      } else if (google.gears) {
+        // Try Google Gears Geolocation
+        browserSupportFlag = true;
+        var geo = google.gears.factory.create('beta.geolocation');
+        geo.getCurrentPosition(function(position) {
+          initialLocation = new google.maps.LatLng(position.latitude,position.longitude);
+          contentString = "Location found using Google Gears";
+          map.setCenter(initialLocation);
+          map.setZoom(16);
+          infowindow.setContent(contentString);
+          infowindow.setPosition(initialLocation);
+          infowindow.open(map);
+        }, function() {
+          handleNoGeolocation(browserSupportFlag);
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        browserSupportFlag = false;
+        handleNoGeolocation(browserSupportFlag);
+      }
+    } else {
+      geocoder.geocode({'address': "Milano"}, function(results, status) {
+        if(status == google.maps.GeocoderStatus.OK) {
+          map.setCenter(results[0].geometry.location);
+        } else {
+          //map.setCenter(new google.maps.LatLng(47, 6));
+        }
+      });
+    }
   }
 
+  function getLatLng(results, status, latlng) {
+      if (status == google.maps.GeocoderStatus.OK)
+        latlng = results[0].geometry.location;
+      else
+        latlng = new google.maps.LatLng(-34.397, 150.644);
+      return latlng;
+  }
   function codeAddress() {
     var address = document.getElementById("address").value;
     geocoder.geocode( { 'address': address}, function(results, status) {
@@ -38,6 +86,9 @@
         alert("Geocode was not successful for the following reason: " + status);
       }
     });
+  }
+  function centerMap() {
+    map.setCenter(codeAddress());
   }
 
 </script>
