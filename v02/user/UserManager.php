3042f7f236f78a08d6aed8a7067c5e26de600425
samvise85 <luca.samvise@gmail.com>
2010-10-02 21:51:31 +0200

    implementato invio mail, validazione mail, login e inserito invio degli errori in UserManager

--- v02/user/UserManager.php
+++ v02/user/UserManager.php
@@ -4,50 +4,84 @@ require_once("settings.php");
 require_once(USER_DIR . "/User.php");
 
 class UserManager{
-    static function createUser($data) {
+	static $UM_NoUserError = "UM_NoUser";
+	static $UM_NoPasswordError = "UM_NoPassword";
+	static $UM_NoSessionError = "UM_NoSession";
+	
+    static function createUser($data, $error = null) {
         $data = Filter::filterArray($data);
         $user = new User($data);
         $u = $user->save();
         //echo "<p>" . $user . "</p>"; //DEBUG
         if($u !== false) {
-            //TODO send verification mail.
-            //TODO crea cartelle mail per spam, inbox e trash
+        	$code = self::generateValidationCode($u);
+        	mail($u->getMail(), "Iscrizione a IoEsisto", self::generateValidationMailMessage($code));
+        	
+			require_once("mail/MailManager.php");
+			MailManager::createDirectory(MAILBOX, $u->getID());
+			MailManager::createDirectory(TRASH, $u->getID());
+			MailManager::createDirectory(SPAM, $u->getID());
         }
         return $user;
     }
     
-    static function verifyUser($user, $code) {
-        $mail = $user->getMail();
-        $password = $user->getPassword();
-        
-        $user->setVerified($code == sha1($mail . $password)); //TODO pensare ed estrarre una funzione che crei questo codice.
+    private static function generateValidationMailMessage($code) {
+    	return '
+    	<h3>Benvenuto in IoEsisto.</h3>
+    	<p>Per verificare la tua email clicca il link qui sotto. Se il tuo client di posta elettronica non supporta i link copia l\'indirizzo qui sotto e incollalo nella barra dell\'indirizzo del tuo browser.</p>
+    	<p><a href="' . $code . '">' . $code . '</a></p> 
+    	';
+    }
+    
+    private static function generateValidationCode($user) {
+    	return sha1($user->getMail() . $user->getPassword());
+    }
+    
+    static function verifyUser($user, $code, $error = null) {
+        $user->setVerified($code == self::generateValidationCode($user));
         return $user->update();
     }
     
-    static function login($data, $pwd) {
-        if(!isset($_POST["nick"])) {
-            ?>
-            <form action="" method="post">
-                
-                
-                
-            </form>
-            <?php
-            
+    static function login($error = null) {
+        if(!isset($_POST["nickname"])) {
+            return self::$UM_NoUserError;
         } else {
-            //TODO check nick e password,
-            //TODO comincia la sessione
-            //TODO registra le variabili
-            //TODO salva cookie
+        	$u = false;
+            //check nick and password
+           	$u = self::loadUserByNickname($_POST["nickname"]);
+           	// assumo che la password mi sia arrivata in chiaro attraverso una connessione sicura
+            if($u !== false && $u->getPassword() == sha1($_POST["password"]))
+            	$logged = true;
+            if($u === false) {
+	       		//check mail and password
+	            $u = self::loadUserByMail($_POST["nickname"]);
+	            // assumo che la password mi sia arrivata in chiaro attraverso una connessione sicura
+            	if($u !== false && $u->getPassword() == sha1($_POST["password"]))
+	            	$logged = true;
+            }
+            if($u === false) {
+	            if($logged) {
+		            //comincia la sessione
+		            if(!session_start())
+		            	return self::$UM_NoSessionError;
+		            //registra le variabili
+		            session_register("user", $u->getNickname());
+		            //TODO salva cookie
+		            return true;
+	            }
+	            return self::$UM_NoPasswordError;
+            }
+            return self::$UM_NoUserError;
         }
-        
     }
     
-    static function logout($session) {
-        //TODO elimina la sessione.
+    static function logout($session, $error = null) {
+        // elimina la sessione.
+        session_unset();
+        session_write_close();
     }
     
-    static function editUser($user, $data) {
+    static function editUser($user, $data, $error = null) {
         require_once("common.php");
         $data = Filter::filterArray($data);
         return $user->edit($data);
@@ -57,14 +91,14 @@ class UserManager{
      * Aggiunge $follower ai followers di $subject e ritorna $follower.
      *
      */
-    static function followUser($follower, $subject) {
+    static function followUser($follower, $subject, $error = null) {
         return $follower->follow($subject);
     }
     
     /**
      * Aggiunge un feedback di $creator a $subject con valore $value e ritorna $creator.
      */
-    static function feedbackUser($creator, $subject, $value) {
+    static function feedbackUser($creator, $subject, $value, $error = null) {
         if($subject->addFeedbackFrom($creator, $value) !== false) {
             $subject->loadFeedback(); //forzo il caricamento del feedback perché sembra non vada…
             return $creator;
@@ -72,43 +106,43 @@ class UserManager{
         return false;
     }
     
-    static function stopFollowingUser($follower, $subject) {
+    static function stopFollowingUser($follower, $subject, $error = null) {
         return $follower->stopFollowing($subject);
     }
     
-    static function deleteFeedbackFromUser($creator, $subject) {
+    static function deleteFeedbackFromUser($creator, $subject, $error = null) {
         //TODO da implementare in User
     }
     
-    static function addContactToUser($data, $user) {
+    static function addContactToUser($data, $user, $error = null) {
         $data[USER] = $user->getID();
         $c = new Contact($data);
         $c->save();
         return $user->addContact($c);
     }
     
-    static function editContact($data, $contact) {
+    static function editContact($data, $contact, $error = null) {
         return $contact->edit($data);
     }
     
-    static function deleteContact($contact, $user) {
+    static function deleteContact($contact, $user, $error = null) {
         return $user->removeContact($contact);
     }
     
-    static function deleteUser($user) {
+    static function deleteUser($user, $error = null) {
         //echo $user;
         return $user->delete();
     }
     
-    static function loadUser($id) {
+    static function loadUser($id, $error = null) {
         return User::loadFromDatabase($id);
     }
     
-    static function loadUserByMail($email) {
+    static function loadUserByMail($email, $error = null) {
         return User::loadByMail($email);
     }
 
-    static function loadUserByNickname($nickname) {
+    static function loadUserByNickname($nickname, $error = null) {
         return User::loadByNickname($nickname);
     }
 }
