5e1e50551bab2329a84b7286e6f65cca6239f84d
dvdcerizza <dvd.cerizza@live.it>
2010-10-07 21:09:58 +0200

    Definita classe MailPage nel file MailPage.php (metodi non ankora implementati)

---
+++ v02/mail/MailPage.php
@@ -0,0 +1,57 @@
+<?php
+	require_once("MailManager.php");
+	
+	class MailPage
+	{
+		static function markLikeSpam() {
+			// TODO
+		}
+
+		static function showMoveToForm() {
+		?>
+			<form name="" action="" method="get"> <!-- TODO -->
+            			<input type="submit" value="">
+        		</form>
+        	<?php		
+			// TODO
+		}		
+		
+		static function showSendForm() {
+		?>
+			<form name="" action="" method="get"> <!-- TODO -->
+            			<input type="submit" value="">
+        		</form>
+        	<?php	
+			// TODO
+		}
+
+		static function showReplyForm() {
+		?>
+			<form name="" action="" method="get"> <!-- TODO -->
+            			<input type="submit" value="">
+        		</form>
+        	<?php	
+			// TODO
+		}
+		
+		static function markLikeRead() {
+			// TODO
+		}
+
+		static function delete() {
+			// TODO
+		}
+
+		static function showDeleted() {
+			// TODO
+		}
+
+		static function showSended() {
+			// TODO
+		}
+
+		static function showRecived() {
+			// TODO
+		}
+	}
+?>
