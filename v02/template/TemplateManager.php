37947c639850890acca48bd43b773879c1afd800
samvise85 <luca.samvise@gmail.com>
2010-10-14 00:32:44 -0700

    Implementato il parser del template ed il validatore (manca da caricare template.xsd), da implementare il salvataggio.

--- v02/template/TemplateManager.php
+++ v02/template/TemplateManager.php
@@ -11,27 +11,65 @@ class TemplateManager {
 		
 	}
 	
+	/**
+	 * @deprecated serve solo come test per il parsing, usare TemplateParser.
+	 * @param unknown_type $filename
+	 */
+	static function parseTemplateFile($filename) {
+		$parser = xml_parser_create();
+		if(!file_exists($filename))
+			echo self::$FILE_NOT_EXISTS;
+		
+		if(xml_parse_into_struct($parser, file_get_contents($filename), $template))
+			echo var_export($template);
+		else echo self::$PARSE_ERROR;
+	}
 }
 
 class TemplateParser {
+	static $NO_ERROR = 0;
 	static $FILE_NOT_EXISTS = 1;
+	static $PARSE_ERROR = 2;
 	
-	static function parseTemplateFile($filename) {
-		if(!file_exists($filename))
+	private $object;
+	private $parser;
+	
+	static function createParser($filename = null) {
+		$tp = new TemplateParser();
+		$tp->parser = xml_parser_create();
+		return $this->setFile($filename);
+	}
+	
+	function setFile($filename) {
+		$this->filename = $filename;
+		return $this;
+	}
+	
+	private $template = null;
+	private $index = 0;
+	
+	function nextElement() {
+		if($template == null)
+			if($err = $this->parseTemplate() != self::$NO_ERROR)
+				return $err;
+		if($index >= count($this->template)) return false;
+		
+		return $this->template[$index];
+	}
+	
+	private function parseTemplate() {
+		if(!file_exists($this->filename))
 			return self::$FILE_NOT_EXISTS;
 		
-
-		$parser = xml_parser_create();
-		
-		while($piece = xml_parse($parser, $tml) !== false) {
-			//TODO
-		}
+		if(xml_parse_into_struct($this->parser, file_get_contents($this->filename), $this->template))
+			return self::$NO_ERROR;
+		return self::$PARSE_ERROR;
 	}
 	
 	static function validateTemplate($templatetxt) {
 		$dom = new DOMDocument();
-		$dom->loadHTML($templatetxt);
-		return $dom->validate();
+		$dom->loadXML($templatetxt);
+		return $dom->schemaValidate("template/template.xsd");
 	}
 	
 	static function saveTemplate($template) {
